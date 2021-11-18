75931f84214ec6daa26081b8ff6e23d26f26d701
Siddharth Bhatnagar <siddharthb1604@gmail.com>
2021-11-18 20:15:07 +0530

    Intial commit

---
+++ main.js
@@ -0,0 +1,70 @@
+const  SHA256  =require("crypto-js/sha256");
+
+class Block{
+    constructor(index,timestamp ,data ,previousHash = ''){
+        this.index=index;
+        this.timestamp=timestamp;
+        this.data=data;
+        this.previousHash =previousHash;
+        this.hash = this.calculateHash();
+        this.nonce=0;
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)+this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0,difficulty) !== Array(difficulty+1).join("0")){
+            this.nonce++;
+            this.hash=this.calculateHash();
+        }
+
+        console.log("Block minded: "+ this.hash);
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain= [this.createGenesisBlock()];
+        this.difficulty=4;
+    }
+
+    createGenesisBlock(){
+        return new Block(0,"01/01/2021","Genesis Block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for(let i= 1;i < this.chain.length; i++){
+            const currentBlock =this.chain[i];
+            const previouBlock=this.chain[i-1];
+
+            if(currentBlock.hash  !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previouBlock.hash){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+let siddharthCoin = new Blockchain();
+
+console.log("Mining Block 1 ....");
+siddharthCoin.addBlock(new Block(1,"18/11/2021", {amount: 4 }));
+console.log("Mining Block 2.....");
+siddharthCoin.addBlock(new Block(2,"18/11/2021", {amount: 6 }));
+
